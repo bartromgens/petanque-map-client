67c4f7fc366099fc2561eacea88e7031aa176adf
Bart Römgens <bart.romgens@gmail.com>
2020-10-31 23:03:50 +0100

    Change matomo url to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: '//api.petanquemap.com',
-  matomoUrl: 'http://piwik.romgens.com/',
+  matomoUrl: 'https://piwik.romgens.com/',
   matomoSiteId: 7
 };
